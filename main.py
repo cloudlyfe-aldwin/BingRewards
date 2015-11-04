cbda6e393d76ec3f043c80922f329e2cbfd85ba9
cloudlyfe-aldwin <cloudlyfe-aldwin@users.noreply.github.com>
2015-11-04 01:57:41 -0500

    Fix unquoted variable string comparison bug

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ from helpers import BingAccountError
 verbose = False
 totalPoints = 0
 
-SCRIPT_VERSION = "3.14.1"
-SCRIPT_DATE = "September 14, 2015"
+SCRIPT_VERSION = "3.14.2"
+SCRIPT_DATE = "November 4, 2015"
 
 def earnRewards(config, httpHeaders, userAgents, reportItem, password):
     """Earns Bing! reward points and populates reportItem"""
